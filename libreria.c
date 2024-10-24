0de3ee71eacddd8171d8beed227f9a3e9ba2a79a
Juanma <juanmabm10@gmail.com>
2024-10-24 17:00:09 +0200

    tail

--- libreria.c
+++ libreria.c
@@ -1,30 +1,61 @@
 #include "libreria.h" // libreria propia con comillas porque es nuestra no de c
-#include <stdio.h>    // manejo de entrada salida
-#include <stdlib.h>   // para poder hacer uso de memoria dinamica
-#include <string.h>   // para poder manejar cadenas string
+#include <stdio.h>    //manejo de entrada salida
+#include <stdlib.h>   //para poder hacer uso de memoria dinamica
+#include <string.h>   //para poder manejar cadenas string
 
-// implementación
 
-int head(int N)
-{
-    char buffer[1024];
+
+
+
+// implementación 
+
+int head(int N){
+
+
+
+
+}
+
+
+
+int tail(int N){
+    int init = 0;
+    char *buffer = (char *)malloc(1024*(sizeof(char)));
     int count = 0;
+    char a[N];
+    int i;
 
-    while (count < N && fgets(buffer, sizeof(buffer), stdin) != NULL)
-    // fgets lee una linea desde la entrada estándar en este caso siendo
-    // buffer donde queremos almacenar los caracteres
-    // sizeof el tamaño que queremos leer
-    // stdin de donde leemos las lineas
-    {
-        printf(% s, buffer); // imprimimos la linea por pantalla
-        count++;             // incrementamos el buffer
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL){
+
+        if (count >= N) {  // despues de las N primeras lineas
+            
+            strcpy(a[init],buffer);
+            init += 1;
+            
+            if (init == N){
+                init = 0;
+            }
+        } else {    //primeras n lineas leidas
+            strcpy(a[count],buffer);
+        }
+
+        count++;
     }
+    
+    for ( init; init < N; init++){
+        printf("%s", a[init]);
+    }
+
+    for ( i = 0; i < init; i++){
+        printf("%s", a[i]);
+    }
+    
+    
 }
 
-int tail(int N)
-{
-}
 
-int longlines(int N)
-{
+
+int longlines(int N){
+
+
 }
