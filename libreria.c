2cb5380575faaaf850a095fd93796cca6d39fdff
teijeiro7 <a@a.com>
2024-10-24 15:07:49 +0200

    Refactor libreria.c: Clean up code formatting and add missing function implementations
    - Cleaned up code formatting in libreria.c for better readability
    - Added missing function implementations for head(), tail(), and longlines()
    
    Added .gitignore file to exclude .vscode folder

--- libreria.c
+++ libreria.c
@@ -1,28 +1,30 @@
 #include "libreria.h" // libreria propia con comillas porque es nuestra no de c
-#include <stdio.h>    //manejo de entrada salida
-#include <stdlib.h>   //para poder hacer uso de memoria dinamica
-#include <string.h>   //para poder manejar cadenas string
+#include <stdio.h>    // manejo de entrada salida
+#include <stdlib.h>   // para poder hacer uso de memoria dinamica
+#include <string.h>   // para poder manejar cadenas string
 
+// implementación
 
-// implementación 
+int head(int N)
+{
+    char buffer[1024];
+    int count = 0;
 
-int head(int N){
-
-
-
-
+    while (count < N && fgets(buffer, sizeof(buffer), stdin) != NULL)
+    // fgets lee una linea desde la entrada estándar en este caso siendo
+    // buffer donde queremos almacenar los caracteres
+    // sizeof el tamaño que queremos leer
+    // stdin de donde leemos las lineas
+    {
+        printf(% s, buffer); // imprimimos la linea por pantalla
+        count++;             // incrementamos el buffer
+    }
 }
 
-
-
-int tail(int N){
-
-
+int tail(int N)
+{
 }
 
-
-
-int longlines(int N){
-
-    
+int longlines(int N)
+{
 }
