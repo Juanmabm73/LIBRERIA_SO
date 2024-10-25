13fcc00b98c190e72a84f72c7772263de7e76e91
teijeiro7 <teijeiroparga2004@gmail.com>
2024-10-25 10:37:24 +0200

    Refactor .gitignore: Add .exe files to ignore list
    Refactor libreria.c: Clean up code formatting and fix function implementations
    
    Refactor test.c: Improve command line argument handling and error messages

--- libreria.c
+++ libreria.c
@@ -4,11 +4,10 @@
 #include <string.h>   //para poder manejar cadenas string
 #include <stdbool.h>
 
+// implementación
 
-
-// implementación 
-
-int head(int N){
+int head(int N)
+{
     char buffer[1024];
     int count = 0;
 
@@ -19,20 +18,16 @@ int head(int N){
     // stdin de donde leemos las lineas
     {
         printf("%s", buffer); // imprimimos la linea por pantalla
-        count++;             // incrementamos el buffer
+        count++;              // incrementamos el buffer
     }
 
     free(buffer);
-
-
-
 }
 
-
-
-int tail(int N){
+int tail(int N)
+{
     int init = 0;
-    char *buffer = (char *)malloc(1024*(sizeof(char)));
+    char *buffer = (char *)malloc(1024 * (sizeof(char)));
     int count = 0;
     char a[N];
     int i;
@@ -41,97 +36,108 @@ int tail(int N){
     // el orden de escritura es hacia la derecha de init si se acaba el array se da una especie de vuelta al principio del array
     // siguiendo en orden hasta init
 
-    while (fgets(buffer, sizeof(buffer), stdin) != NULL){
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
 
-        if (count >= N) {      // despues de las N primeras lineas
-            
-            strcpy(a[init],buffer);
-            init += 1;        // avanzamos init a la siguiente frase
-            
-            if (init == N){   // si llegamos al final del array con init el inicio pasa al principio
+        if (count >= N)
+        { // despues de las N primeras lineas
+
+            strcpy(a[init], buffer);
+            init += 1; // avanzamos init a la siguiente frase
+
+            if (init == N)
+            { // si llegamos al final del array con init el inicio pasa al principio
                 init = 0;
             }
-
-        } else {    //primeras n lineas leidas
-            strcpy(a[count],buffer);
+        }
+        else
+        { // primeras n lineas leidas
+            strcpy(a[count], buffer);
         }
 
         count++;
     }
 
-
     // impresion por salida estandar
 
-    if (count < N){
-        for (i=0; i<count; i++){
+    if (count < N)
+    {
+        for (i = 0; i < count; i++)
+        {
             printf("%s", a[i]);
         }
-
-    } else {
-        for ( init; init < N; init++){ // de init al final 
-        printf("%s", a[init]);
+    }
+    else
+    {
+        for (init; init < N; init++)
+        { // de init al final
+            printf("%s", a[init]);
         }
 
-        for ( i = 0; i < init; i++){   // desde el inicio hasta init
+        for (i = 0; i < init; i++)
+        { // desde el inicio hasta init
             printf("%s", a[i]);
         }
     }
 
-
-    // vaciamos el buffer y el array 
-    for ( i = 0; i < N; i++){
+    // vaciamos el buffer y el array
+    for (i = 0; i < N; i++)
+    {
         free(a[i]);
     }
-    
-    free(buffer);
 
+    free(buffer);
 }
 
-
-
-int longlines(int N) {
+int longlines(int N) // imprime las N lineas mas largas de todo el fichero
+{
     int j;
-    char *buffer = (char *)malloc(1024*(sizeof(char)));
+    char *buffer = (char *)malloc(1024 * (sizeof(char)));
     char a[N];
     int i = 0;
     int len;
-    
-    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        
-        len = strlen(buffer);    // nos guardamos la longitud de la siguiente linea 
-        
-         
-        if (i< N) { //array sin llenar 
-            j = i-1;
-            while (j >= 0 && len > strlen(a[j])){
-                strcpy(a[j+1], buffer); // adelantamos una posicion el mas pequeño para reemplazarlo fuera del bucle por el grande
-                j--;                    // vamos al anterior
+
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+
+        len = strlen(buffer); // nos guardamos la longitud de la siguiente linea
+
+        if (i < N)
+        { // array sin llenar
+            j = i - 1;
+            while (j >= 0 && len > strlen(a[j]))
+            {
+                strcpy(a[j + 1], buffer); // adelantamos una posicion el mas pequeño para reemplazarlo fuera del bucle por el grande
+                j--;                      // vamos al anterior
             }
 
-            strcpy(a[j+1], buffer);     // introducimos el valor mas grande en su sitio
+            strcpy(a[j + 1], buffer); // introducimos el valor mas grande en su sitio
             i++;
-        } else {
-            
-            if (len > strlen(a[N])){    // vaciamos el ultimo hueco del array
+        }
+        else
+        {
+
+            if (len > strlen(a[N]))
+            { // vaciamos el ultimo hueco del array
                 free(a[N]);
             }
-            j = N -2; // para empezar desde el penultimo
+            j = N - 2; // para empezar desde el penultimo
 
-            while (j >= 0 && len > strlen(a[j])){
-                strcpy(a[j+1], a[j]);
+            while (j >= 0 && len > strlen(a[j]))
+            {
+                strcpy(a[j + 1], a[j]);
                 j--;
             }
 
-            strcpy(a[j+1], buffer);
+            strcpy(a[j + 1], buffer);
             i++;
         }
-
     }
 
-
-    for ( j = 0; j < i ; j++){
+    for (j = 0; j < i; j++)
+    {
         printf("%s", a[i]);
-        free(a[i]); //vaciamos a la vez que mostramos
+        free(a[i]); // vaciamos a la vez que mostramos
     }
 
     free(buffer);
